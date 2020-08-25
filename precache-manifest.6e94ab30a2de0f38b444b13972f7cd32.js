7a4085eff631c803b98c2480af345912e9e6b7ce
Irene Beauregard <irenebeauregard@Irenes-MBP.fios-router.home>
2020-08-25 13:51:23 -0400

    Updates

---
+++ precache-manifest.6e94ab30a2de0f38b444b13972f7cd32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ac6611cd23b0cca0aff99b06ef93a3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "55d1af7675ae97e347d8",
+    "url": "/monsters-rolodex/static/css/main.9cac793e.chunk.css"
+  },
+  {
+    "revision": "ccbd263f033df4ef2e8f",
+    "url": "/monsters-rolodex/static/js/2.0704fbba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.0704fbba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55d1af7675ae97e347d8",
+    "url": "/monsters-rolodex/static/js/main.c9a8a1e3.chunk.js"
+  },
+  {
+    "revision": "cfddd26a051fc67f1c3e",
+    "url": "/monsters-rolodex/static/js/runtime-main.54751967.js"
+  }
+]);
